bb13cce72cb880a8cf26a853d92ddd4ed45cf9f7
frenchh-coral <nimarquoi2@gmail.com>
2026-07-02 09:31:01 +0200

    lost pages fix

--- src/app/privacy-policy/page.js
+++
@@ -1,8 +0,0 @@
-export default function PrivacyPolicyPage() {
-  return (
-    <main style={styles.container}>
-      <h1>Privacy Policy</h1>
-      <p>Comming sometimes soon</p>
-    </main>
-  );
-}
